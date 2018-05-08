8e2af7f379323fc0d75ea859cc03b01dbe250016
alex65535 <4252514@gmail.com>
2018-05-08 15:30:16 +0800

    增加 nextcloud 图片视频上传程序

---
+++ nextcloud_up_pic_movie.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+
+#movie_name = os.listdir('Z:\\家庭照片视频\\手机备份\\simon手机备份 2015-5-10\\DCIM\\Camera'.decode('utf-8').encode(sys.getfilesystemencoding()))
+pic_path = 'Z:\\家庭照片视频\\手机备份\\simon手机备份 2015-5-10\\DCIM\\Camera'
+movie_name = os.listdir(pic_path)
+print movie_name
+我 = 1
